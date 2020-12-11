8036a046c8bf3f62d8d522ec32f3d9396c3a833a
David Guevara <datwaftx@gmail.com>
2020-12-11 11:31:41 -0600

    Bugfix: invalid label

--- lua/bubbly/plugin.lua
+++ lua/bubbly/plugin.lua
@@ -3,9 +3,9 @@
 -- ==================
 -- Created by: datwaft [github.com/datwaft]
 
--- ============
--- Finalization
--- ============
+-- ==========
+-- Definition
+-- ==========
    return function(inactive)
       if inactive and type(inactive) ~= 'boolean' then inactive = true end
       return require'bubbly.factories.statusline'(vim.g.bubbly_statusline, inactive)
