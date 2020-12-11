5fdf1675875311de7845ac3e85f4cd87cbb9ba85
David Guevara <datwaftx@gmail.com>
2020-12-11 13:43:18 -0600

    Bugfix: fixed branch bubble when no branch

--- lua/bubbly/components/branch.lua
+++ lua/bubbly/components/branch.lua
@@ -7,8 +7,12 @@ local bubble_factory = require'bubbly.factories.bubble'
 
 return function(inactive)
    if inactive then return '' end
+   local data = vim.b.git_branch
+   if data ~= '' then
+      data = vim.g.bubbly_symbols.branch .. data
+   end
    return bubble_factory{{
-      data = vim.g.bubbly_symbols.branch .. vim.b.git_branch,
+      data = data,
       color = vim.g.bubbly_colors.branch,
       style = vim.g.bubbly_styles.branch,
    }}
