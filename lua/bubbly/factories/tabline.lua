616b7a71ac7607ef8ccf29f33dd76a8a51c7e4a6
David Guevara <datwaftx@gmail.com>
2020-12-17 17:52:13 -0600

    Bugfix: added tabline module

---
+++ lua/bubbly/factories/tabline.lua
@@ -0,0 +1,24 @@
+-- ===========================
+-- BUBBLY.NVIM TABLINE FACTORY
+-- ===========================
+-- Created by: datwaft [github.com/datwaft]
+
+local bubble_factory = require'bubbly.factories.bubble'
+
+return function()
+   local this = vim.fn.tabpagenr()
+   local tabline = {}
+   for i = 1, vim.fn.tabpagenr('$') + 1 do
+      local tabname = vim.fn.bufname(vim.fn.tabpagebuflist(i)[vim.fn.tabpagewinnr(i)])
+      local color = this == i and vim.g.bubbly_colors.tabline.active or vim.g.bubbly_colors.tabline.inactive
+      local style = this == i and vim.g.bubbly_styles.tabline.active or vim.g.bubbly_styles.tabline.inactive
+      tabline[#tabline] = {
+         { pre = '%'..i..'T', data = tabname, post = '%T', color = color, style = style },
+         { pre = '%'..i..'X', data = vim.g.bubbly_characters.close, post = '%X', color = 'darkgrey' },
+      }
+   end
+   local result = ''
+   for _, e in ipairs(tabline) do
+      result = result .. bubble_factory(e) .. ' '
+   end
+end
