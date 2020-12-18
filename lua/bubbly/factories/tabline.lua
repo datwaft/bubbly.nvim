4f733883e94ef34c849025f2f82adb5254bfb49b
David Guevara <datwaftx@gmail.com>
2020-12-17 18:07:19 -0600

    Bugfix: added return

--- lua/bubbly/factories/tabline.lua
+++ lua/bubbly/factories/tabline.lua
@@ -8,22 +8,21 @@ local bubble_factory = require'bubbly.factories.bubble'
 return function()
    local this = vim.fn.tabpagenr()
    local tabline = {}
-   for i = 1, vim.fn.tabpagenr('$') + 1 do
+   for i = 1, vim.fn.tabpagenr('$') do
       local buflist = vim.fn.tabpagebuflist(i)
-      if type(buflist) == 'table' then
-         local winnr = vim.fn.tabpagewinnr(i)
-         local bufnr = buflist[winnr]
-         local tabname = vim.fn.bufname(bufnr)
-         local color = this == i and vim.g.bubbly_colors.tabline.active or vim.g.bubbly_colors.tabline.inactive
-         local style = this == i and vim.g.bubbly_styles.tabline.active or vim.g.bubbly_styles.tabline.inactive
-         tabline[#tabline] = {
-            { pre = '%'..i..'T', data = tabname, post = '%T', color = color, style = style },
-            { pre = '%'..i..'X', data = vim.g.bubbly_characters.close, post = '%X', color = 'darkgrey' },
-         }
-      end
+      local winnr = vim.fn.tabpagewinnr(i)
+      local bufnr = buflist[winnr]
+      local tabname = vim.fn.bufname(bufnr)
+      local color = this == i and vim.g.bubbly_colors.tabline.active or vim.g.bubbly_colors.tabline.inactive
+      local style = this == i and vim.g.bubbly_styles.tabline.active or vim.g.bubbly_styles.tabline.inactive
+      tabline[#tabline] = {
+         { pre = '%'..i..'T', data = tabname, post = '%T', color = color, style = style },
+         { pre = '%'..i..'X', data = vim.g.bubbly_characters.close, post = '%X', color = 'darkgrey' },
+      }
    end
    local result = ''
    for _, e in ipairs(tabline) do
       result = result .. bubble_factory(e) .. ' '
    end
+   return result
 end
