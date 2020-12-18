9e0939767d5f1110995cf3c96d97ddb6248e5107
David Guevara <datwaftx@gmail.com>
2020-12-17 18:05:36 -0600

    Bugfix: separated the variables

--- lua/bubbly/factories/tabline.lua
+++ lua/bubbly/factories/tabline.lua
@@ -8,17 +8,19 @@ local bubble_factory = require'bubbly.factories.bubble'
 return function()
    local this = vim.fn.tabpagenr()
    local tabline = {}
-   for i = 1, vim.fn.tabpagenr('$') do
+   for i = 1, vim.fn.tabpagenr('$') + 1 do
       local buflist = vim.fn.tabpagebuflist(i)
-      local winnr = vim.fn.tabpagewinnr(i)
-      local bufnr = buflist[winnr]
-      local tabname = vim.fn.bufname(bufnr)
-      local color = this == i and vim.g.bubbly_colors.tabline.active or vim.g.bubbly_colors.tabline.inactive
-      local style = this == i and vim.g.bubbly_styles.tabline.active or vim.g.bubbly_styles.tabline.inactive
-      tabline[#tabline] = {
-         { pre = '%'..i..'T', data = tabname, post = '%T', color = color, style = style },
-         { pre = '%'..i..'X', data = vim.g.bubbly_characters.close, post = '%X', color = 'darkgrey' },
-      }
+      if type(buflist) == 'table' then
+         local winnr = vim.fn.tabpagewinnr(i)
+         local bufnr = buflist[winnr]
+         local tabname = vim.fn.bufname(bufnr)
+         local color = this == i and vim.g.bubbly_colors.tabline.active or vim.g.bubbly_colors.tabline.inactive
+         local style = this == i and vim.g.bubbly_styles.tabline.active or vim.g.bubbly_styles.tabline.inactive
+         tabline[#tabline] = {
+            { pre = '%'..i..'T', data = tabname, post = '%T', color = color, style = style },
+            { pre = '%'..i..'X', data = vim.g.bubbly_characters.close, post = '%X', color = 'darkgrey' },
+         }
+      end
    end
    local result = ''
    for _, e in ipairs(tabline) do
