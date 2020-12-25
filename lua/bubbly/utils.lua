2ad21ff19e466d9371fecf4f068dca558a8c729a
datwaft <datwaftx@gmail.com>
2020-12-24 23:55:24 -0600

    Bugfix: add invalid special adjustment

--- lua/bubbly/utils.lua
+++ lua/bubbly/utils.lua
@@ -33,6 +33,7 @@
 -- ==================
    M.gethighlight = function(foreground, background, special)
       if not foreground then foreground = '' end
+      if not special then special = '' end
       if foreground == 'dark' then
          return 'Bubbly'..M.titlecase(background)..'Dark'..M.titlecase(special)
       else
