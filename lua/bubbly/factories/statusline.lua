75492cfae1d8fba43dedccdd24626eeab14563ec
datwaft <datwaftx@gmail.com>
2021-02-08 20:50:56 -0600

    [Enhancement] Reworked statusline factory

--- lua/bubbly/factories/statusline.lua
+++ lua/bubbly/factories/statusline.lua
@@ -4,35 +4,52 @@
 -- Created by datwaft <github.com/datwaft>
 
 local bubble_factory = require'bubbly.factories.bubble'
+local filter = require'bubbly.utils.table'.filter
 
-return function(list, inactive)
+local construct_statusline = function(list)
    local statusline = ''
-   local left = true
-   for _, e in ipairs(list) do
-      local type = type(e)
-      if type == 'table' then
-         if not left then statusline = statusline..' ' end
-         statusline = statusline..bubble_factory(e)
-         if left then statusline = statusline..' ' end
-      elseif type == 'function' then
-         if not left then statusline = statusline..' ' end
-         statusline = statusline..bubble_factory(e(inactive))
-         if left then statusline = statusline..' ' end
-      elseif type == 'string' then
-         if left and e:lower() == 'divisor' or e:lower() == 'division' then
-            statusline = statusline..'%='
-            left = false
-         elseif e:lower() == 'truc' or e:lower() == 'truncate' then
-            statusline = statusline..'%<'
-         else
-            local component = require'bubbly.utils.prerequire'('bubbly.components.'..e:lower())
-            if component then
-               if not left then statusline = statusline..' ' end
-               statusline = statusline..bubble_factory(component(inactive))
-               if left then statusline = statusline..' ' end
-            end
+   local alignment = 'left'
+   for i, bubble in ipairs(list) do
+      if bubble == '%=' then
+         alignment = 'right'
+      elseif bubble == '%<' then
+         statusline = statusline..bubble
+      elseif alignment == 'left' then
+         if i ~= 1 then
+            statusline = statusline..' '
+         end
+         statusline = statusline..bubble
+      else
+         statusline = statusline..bubble
+         if i ~= #list then
+            statusline = statusline..' '
          end
       end
    end
    return statusline
+end
+
+return function(list, inactive)
+   local statusline = {}
+   for _, e in ipairs(list) do
+      local type = type(e)
+      if type == 'table' then
+         statusline[#statusline + 1] = bubble_factory(e)
+      elseif type == 'function' then
+         statusline[#statusline + 1] = bubble_factory(e(inactive))
+      elseif type == 'string' then
+         if e:lower() == 'divisor' or e:lower() == 'division' then
+            statusline[#statusline + 1] = '%='
+         elseif e:lower() == 'truc' or e:lower() == 'truncate' then
+            statusline[#statusline + 1] = '%<'
+         else
+            local component = require'bubbly.utils.prerequire'('bubbly.components.'..e:lower())
+            if component then
+               statusline[#statusline + 1] = bubble_factory(component(inactive))
+            end
+         end
+      end
+   end
+   statusline = filter(statusline, function(_, e) return type(e) ~= 'string' or e == '' end)
+   return construct_statusline(statusline)
 end
