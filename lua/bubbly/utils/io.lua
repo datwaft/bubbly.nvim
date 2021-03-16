730911cccb333072c57b6e73ef273cb06a302d10
datwaft <datwaftx@gmail.com>
2021-03-16 15:42:07 -0600

    Better IO Utils

--- lua/bubbly/utils/io.lua
+++ lua/bubbly/utils/io.lua
@@ -13,22 +13,40 @@
 -- Warning
 -- =======
 
-   -- Output a warning message
-   M.warning = function(message)
+   -- Outputs a raw warning message
+   M.raw_warning = function(message)
       vim.cmd([[echohl WarningMsg]])
       vim.cmd(string.format([[echomsg "%s"]], vim.fn.escape(message, "\"\\")))
       vim.cmd([[echohl None]])
+   end
+
+   -- Outputs a warning message
+   -- Prepends "[BUBBLY.NVIM] => [WARNING] " to the warning message
+   M.warning = function(message)
+     vim.cmd([[echohl WarningMsg]])
+     vim.cmd(string.format([[echomsg "[BUBBLY.NVIM] [WARNING] %s"]],
+       vim.fn.escape(message, "\"\\")))
+     vim.cmd([[echohl None]])
    end
 
 -- =====
 -- Error
 -- =====
 
-   -- Output an error message
-   M.error = function(message)
+   -- Outputs a raw error message
+   M.raw_error = function(message)
       vim.cmd([[echohl ErrorMsg]])
       vim.cmd(string.format([[echomsg "%s"]], vim.fn.escape(message, "\"\\")))
       vim.cmd([[echohl None]])
+   end
+
+   -- Outputs an error message
+   -- Prepends "[BUBBLY.NVIM] => [ERROR] " to the error message
+   M.error = function(message)
+     vim.cmd([[echohl ErrorMsg]])
+     vim.cmd(string.format([[echomsg "[BUBBLY.NVIM] [ERROR] %s"]],
+       vim.fn.escape(message, "\"\\")))
+     vim.cmd([[echohl None]])
    end
 
 -- ============
